9ad3700f51a8defd9cdc41eae2bcfc8d57b2c6ee
manigorsh <manigorsh@gmail.com>
2017-11-07 20:52:05 +0200

    added login form

--- catalog/views.py
+++ catalog/views.py
@@ -17,19 +17,30 @@ def index(request):
     num_authors = Author.objects.count()  # The 'all()' is implied by default.
     num_genres = Genre.objects.count()  # The 'all()' is implied by default.
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
         context={'num_books': num_books, 'num_instances': num_instances,
-                 'num_instances_available': num_instances_available, 'num_authors': num_authors, 'num_genres': num_genres},
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors, 'num_genres': num_genres,
+                 'num_visits':num_visits},
     )
 
 from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 1
+    paginate_by = 10
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
