51b5f00528983534312cc8d30073ced4142ce6fa
Igor Mansurov <igorm@crow.co.il>
2017-11-20 13:32:21 +0200

    switched to aws postgress

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -92,13 +92,24 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'walladb',
+        'USER': 'walladbuser',
+        'PASSWORD': 'NDnhca893jucuiscsn',
+        'HOST': 'walladbinstance.cbk3yjyvvox2.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
